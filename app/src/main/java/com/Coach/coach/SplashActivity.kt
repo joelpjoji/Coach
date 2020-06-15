55bca4834830d0995bb8aa8d1c41411e3dac4fb4
joelpjoji <joelpjoji1222@gmail.com>
2020-06-15 15:33:30 +0530

    login layout

--- app/src/main/java/com/Coach/coach/SplashActivity.kt
+++ app/src/main/java/com/Coach/coach/SplashActivity.kt
@@ -3,15 +3,26 @@ package com.Coach.coach
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
 import androidx.constraintlayout.motion.widget.MotionLayout
 
 
 
 public class SplashActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            /* Create an Intent that will start the Menu-Activity. */
+
+            val mainIntent = Intent(this, LoginActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+
+        }, 3000)
     }
     fun  onTransitionChange (motionLayout: MotionLayout){
         motionLayout.setTransitionListener( object: MotionLayout.TransitionListener {
