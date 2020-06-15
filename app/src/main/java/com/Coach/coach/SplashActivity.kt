01941b260cb9919a435ca327992f47b44b62f632
joelpjoji <joelpjoji1222@gmail.com>
2020-06-15 13:58:53 +0530

    1

--- app/src/main/java/com/Coach/coach/SplashActivity.kt
+++ app/src/main/java/com/Coach/coach/SplashActivity.kt
@@ -3,34 +3,37 @@ package com.Coach.coach
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.core.content.ContextCompat.startActivity
+
 
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        fun onTransitionChange(motionLayout: MotionLayout) {
+            motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+
+                override fun onTransitionTrigger(
+                    p0: MotionLayout?,
+                    p1: Int,
+                    p2: Boolean,
+                    p3: Float
+                ) {
+                }
+
+                override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                }
+
+                override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                }
+
+                override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                    startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                }
+            })
+        }
     }
-    fun  onTransitionChange (motionLayout: MotionLayout,
-    start: Int,
-    end: Int,
-    progress: Float){
-        motionLayout.setTransitionListener(
-            object: MotionLayout.TransitionListener {
-
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
-
-            }
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-            }
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
-            }
-
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-            }
-        })
     }
-}
+
