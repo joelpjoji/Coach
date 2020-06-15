b73520cffa45007a46aea8c58d5bb3a99e40991f
joelpjoji <joelpjoji1222@gmail.com>
2020-06-15 13:30:52 +0530

    1

--- app/src/main/java/com/Coach/coach/LoginActivity.kt
+++ app/src/main/java/com/Coach/coach/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.Coach.coach
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.content.ContextCompat.startActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
+
+
+
+
+
+
+
+
