7ec27706d6a80efe000e8807206b32c9bf90a0ce
joelpjoji <joelpjoji1222@gmail.com>
2020-06-15 10:52:08 +0530

    1

---
+++ app/src/main/java/com/Coach/coach/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.Coach.coach
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import java.util.*
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
