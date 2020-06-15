1aecb87835b9003d2894156dda1f3fb96838a896
0shaheen0 <shaheensheik007@gmail.com>
2020-06-15 13:31:12 +0530

    Added UserProfile and Instrument Activity

---
+++ app/src/main/java/com/Coach/coach/InstrumentsActivity.kt
@@ -0,0 +1,11 @@
+package com.Coach.coach
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InstrumentsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instruments)
+    }
+}
