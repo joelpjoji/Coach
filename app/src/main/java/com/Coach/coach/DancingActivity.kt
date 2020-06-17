9c630f5c3ac54f11dc00cfb33274e6da2efaab03
joelpjoji <joelpjoji1222@gmail.com>
2020-06-17 18:29:46 +0530

    Added All the Activity for the Home Activity

---
+++ app/src/main/java/com/Coach/coach/DancingActivity.kt
@@ -0,0 +1,11 @@
+package com.Coach.coach
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DancingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dancing)
+    }
+}
---
+++ app/src/main/java/com/Coach/coach/SingingActivity.kt
@@ -0,0 +1,11 @@
+package com.Coach.coach
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singing)
+    }
+}
