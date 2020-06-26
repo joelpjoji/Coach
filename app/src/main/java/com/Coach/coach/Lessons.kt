24f5ea2cfb1087426fc2656ab5c67e288ff11ae5
0shaheen0 <shaheensheik007@gmail.com>
2020-06-26 14:11:31 +0530

    Prog bar to lesson

--- app/src/main/java/com/Coach/coach/Lessons.kt
+++ app/src/main/java/com/Coach/coach/Lessons.kt
@@ -1,11 +1,23 @@
 package com.Coach.coach
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_lessons.*
 
 class Lessons : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lessons)
+
+        progress_circular.apply{
+            progressMax = 100f
+            setProgressWithAnimation(50f,1000)
+            progressBarWidth = 5f
+            progressBarColor = Color.CYAN
+            backgroundProgressBarWidth= 7f
+
+
+        }
     }
 }
