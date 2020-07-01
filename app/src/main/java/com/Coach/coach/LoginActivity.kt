697f5824d08a13627825d43186badb32fbf7f013
joelpjoji <joelpjoji1222@gmail.com>
2020-07-01 16:16:54 +0530

    Added Terms and Conditions on the login Screen , clicking that goes to a temporary site#

--- app/src/main/java/com/Coach/coach/LoginActivity.kt
+++ app/src/main/java/com/Coach/coach/LoginActivity.kt
@@ -2,7 +2,13 @@ package com.Coach.coach
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.text.method.MovementMethod
+import android.text.util.Linkify
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
@@ -17,6 +23,9 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.view.*
+import java.util.Calendar.getInstance
+import java.util.Currency.getInstance
 
 class LoginActivity : AppCompatActivity() {
 
@@ -32,6 +41,35 @@ class LoginActivity : AppCompatActivity() {
         configureGoogleSignIn()
         setupUI()
         firebaseAuth = FirebaseAuth.getInstance()
+
+        val textView = (findViewById<View>(R.id.terms) as TextView).also({
+            it.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(view: View): Unit {
+                    // Handler code here.
+                    val intent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://hastebin.com/utixekogaj.sql")
+                    );
+                    startActivity(intent);
+                }
+
+
+            })
+        })
+        val textView1 = (findViewById<View>(R.id.conditions) as TextView).also({
+            it.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(view: View): Unit {
+                    // Handler code here.
+                    val intent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://hastebin.com/utixekogaj.sql")
+                    );
+                    startActivity(intent);
+                }
+
+
+            })
+        })
     }
 
     companion object {
